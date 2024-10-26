9944f65f688513e6acdc5d3f5033dfa615801453
tanakadaichi_1989 <tanakadaichi_1989@icloud.com>
2024-10-26 22:49:18 +0900

    edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,68 @@ import Footer from './Footer';
 import Button from './Button';
 import Separator from './Separator';
 
+interface IEvent {
+  name: string
+}
+
+interface ITopic {
+  photoURL: string,
+  kind: string,
+  title: string
+}
+
+interface INews {
+  photoURL: string,
+  kind: string,
+  title: string
+}
+
 function App() {
+  let events: IEvent[] = [
+    { name: "event1" },
+    { name: "event2" }
+  ]
+
+  let topics: ITopic[] = [
+    { photoURL: "#", kind: "topic-kind1", title: "Topic1" },
+    { photoURL: "#", kind: "topic-kind2", title: "Topic2" },
+    { photoURL: "#", kind: "topic-kind3", title: "Topic3" }
+  ]
+
+  let newsList: INews[] = [
+    { photoURL: "#", kind: "news-kind1", title: "News1" },
+    { photoURL: "#", kind: "news-kind2", title: "News2" },
+    { photoURL: "#", kind: "news-kind3", title: "News3" }
+  ]
+
   return (
     <div className="App">
       <Header />
       <div className = "eventComponent">
-        <Event name = "event1" />
+        {
+          events.map((event) => {
+            return <Event name = {event.name} />
+          })
+        }
+        <Button text = "button1" linkURL = "#" />
       </div>
       <Separator />
-      <Button text = "button1" linkURL = "#" />
       <div className = "topicComponent">
-        <Topic name = "topic1" />
+        {
+          topics.map((topic) => {
+            return <Topic name = {topic.title} />
+          })
+        }
+        <Button text = "button2" linkURL = "#" />
       </div>
       <Separator />
       <div className = "newsComponent">
-        <News name = "news1" />
+        {
+          newsList.map((news) => {
+            return <News name = {news.title} />
+          })
+        }
+        <Button text = "button3" linkURL = "#" />
       </div>
       <Separator />
       <Footer />
