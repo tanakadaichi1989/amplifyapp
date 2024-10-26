c6e05ca4253eb0d0aefcc1deb3fb6f7c6e23d13c
tanakadaichi_1989 <tanakadaichi_1989@icloud.com>
2024-10-26 21:36:15 +0900

    add Footer.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render(){
+        return(
+            <div>
+                <h2>AmplifyApp</h2>
+            </div>
+        )
+    }
+}
+
+export default Footer;
