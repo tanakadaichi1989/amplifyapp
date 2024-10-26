1f2122b485dc6a497c4b1a61719b66998174d991
tanakadaichi_1989 <tanakadaichi_1989@icloud.com>
2024-10-26 22:37:56 +0900

    add Separator.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Topic from './Topic';
 import News from './News';
 import Footer from './Footer';
 import Button from './Button';
+import Separator from './Separator';
 
 function App() {
   return (
@@ -15,13 +16,16 @@ function App() {
       <div className = "eventComponent">
         <Event name = "event1" />
       </div>
+      <Separator />
       <Button text = "button1" linkURL = "#" />
       <div className = "topicComponent">
         <Topic name = "topic1" />
       </div>
+      <Separator />
       <div className = "newsComponent">
         <News name = "news1" />
       </div>
+      <Separator />
       <Footer />
     </div>
   );
---
+++ src/Separator.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Separator extends React.Component {
+    render(){
+        return(
+            <div className="separator"></div>
+        );
+    }
+}
+
+export default Separator;
