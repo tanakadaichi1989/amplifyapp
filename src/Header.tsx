763647937c3e04bf5ee59c3c674062b07eddd99a
tanakadaichi_1989 <tanakadaichi_1989@icloud.com>
2024-10-26 22:10:26 +0900

    Edit App.css

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Header extends React.Component {
     render(){
         return(
-            <div>
+            <div className = "Header">
                 <h1>AmplifyApp</h1>
             </div>
         )
