324d18a2a6540c9e906524c1f89518e87c9c058c
tanakadaichi_1989 <tanakadaichi_1989@icloud.com>
2024-10-26 23:12:56 +0900

    edit Footer.tsx and App.css regarding to Footer component

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 class Footer extends React.Component {
     render(){
         return(
-            <div>
+            <div className = "Footer">
                 <h2>AmplifyApp</h2>
+                <p>This page was made of React.js and AWS Amplify.</p>
             </div>
         )
     }
